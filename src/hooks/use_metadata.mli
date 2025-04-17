0bdba49f1f3a0c9be6fef5ea21be7229887f6fbf
0x6A6471 <hello@jakequinter.io>
2025-04-17 09:24:49 -0500

    cleanup

--- src/hooks/use_metadata.mli
+++ src/hooks/use_metadata.mli
@@ -1,3 +1,3 @@
-external set_document_title : string -> unit = "setDocumentTitle"
+external set_document_title' : string -> unit = "setDocumentTitle"
 
-val use_document_title : string -> unit
+val set_document_title : string -> unit
